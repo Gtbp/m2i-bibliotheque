d7fc8909b65f824a3d598e49149d71d9529f78b7
Thibaud Glutron <thibaud.glutron@gmail.com>
2023-07-26 16:32:29 +0200

    creation entites branche Thib

--- filRouge/src/main/java/com/m2i/filRouge/entities/Personne.java
+++ filRouge/src/main/java/com/m2i/filRouge/entities/Personne.java
@@ -1,5 +1,16 @@
 package com.m2i.filRouge.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
 public class Personne {
 
+	private String prenom;
+	private String nom;
+	private String email;
+	private String telephone;
+	private String adresse;
 }
