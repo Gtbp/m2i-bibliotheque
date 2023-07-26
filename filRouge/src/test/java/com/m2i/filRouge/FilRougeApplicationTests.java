0d7c4dfbe2fb054704972610a9e1b27187a474c6
Raphael Frn <raphfar@gmail.com>
2023-07-26 15:57:07 +0200

    projet fil rouge

---
+++ filRouge/src/test/java/com/m2i/filRouge/FilRougeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m2i.filRouge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilRougeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
