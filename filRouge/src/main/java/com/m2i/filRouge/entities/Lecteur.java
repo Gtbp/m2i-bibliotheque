5f0105895f3890163bd82926b762db6c244a1205
Thibaud Glutron <thibaud.glutron@gmail.com>
2023-07-27 10:13:14 +0200

    ajout annotations lecteur, admin, personne

--- filRouge/src/main/java/com/m2i/filRouge/entities/Lecteur.java
+++ filRouge/src/main/java/com/m2i/filRouge/entities/Lecteur.java
@@ -1,13 +1,22 @@
 package com.m2i.filRouge.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor
 public class Lecteur extends Personne{
 
-
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="idLecteur")
 	private Long idLecteur;
 }
--- filRouge/src/main/java/com/m2i/filRouge/idao/dao/DaoLecteur.java
+++ filRouge/src/main/java/com/m2i/filRouge/idao/dao/DaoLecteur.java
@@ -1,10 +1,29 @@
 package com.m2i.filRouge.idao.dao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Repository;
+
 import com.m2i.filRouge.entities.Lecteur;
 import com.m2i.filRouge.idao.IDaoLecteur;
 
+@Repository
+@Transactional
 public class DaoLecteur extends DaoGeneric<Lecteur, Long> implements IDaoLecteur  {
-
 	
+	@PersistenceContext
+    private EntityManager entityManager;
+
+	@Override
+	public EntityManager getEntityManager() {
+		// TODO Auto-generated method stub
+		return entityManager;
+	}
+	
+	public DaoLecteur() {
+		super(Lecteur.class);
+	}
 
 }
