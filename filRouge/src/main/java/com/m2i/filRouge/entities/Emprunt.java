79071da7e38f1c08e19a996445dbcebd4b2eed01
Raphael Frn <raphfar@gmail.com>
2023-07-27 16:39:49 +0200

    test emprunt all except prolonger

--- filRouge/src/main/java/com/m2i/filRouge/entities/Emprunt.java
+++ filRouge/src/main/java/com/m2i/filRouge/entities/Emprunt.java
@@ -9,17 +9,14 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 
 @Entity
-@Getter @Setter @ToString @NoArgsConstructor @AllArgsConstructor
+@Getter @Setter @NoArgsConstructor 
 public class Emprunt {
 	
 	public enum TypesEmprunt {RESERVATION, EFFECTIF};
@@ -39,16 +36,27 @@ public class Emprunt {
 	private TypesEmprunt type;
 	
 	@ManyToOne
-		@JoinColumn(name= "lecteur")
+		@JoinColumn(name= "idLecteur")
 	private Lecteur lecteur;
 	
 	@OneToOne(optional=false)
-		@MapsId
 			@JoinColumn(name="idLivre")  // pas sûr de l'id ici
 	private Livre livre;
 
 	@OneToOne
-		@MapsId
 			@JoinColumn(name="motif")
 	private Incident incident;
+
+	public Emprunt(Long idEmprunt, Date date_debut, Date date_fin, TypesEmprunt type, Lecteur lecteur, Livre livre) {
+		super();
+		this.idEmprunt = idEmprunt;
+		this.date_debut = date_debut;
+		this.date_fin = date_fin;
+		this.type = type;
+		this.lecteur = lecteur;
+		this.livre = livre;
+	}
+	
+	
+	
 }
